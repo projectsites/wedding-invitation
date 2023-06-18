e35757880c6911317d4f73a95d10e5a3b6162bf6
JM Gabriel <johnmark@sdsolutions.com.ph>
2023-06-18 20:19:40 +0800

    - fix and apply favicon - change theme color - declare global variables for color primary and secondary to easily change theme - styled story section

--- js/scripts.js
+++ js/scripts.js
@@ -230,24 +230,8 @@ function parallaxBackground() {
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-
-    //I'm adding this section so I don't have to keep updating this pen every year :-)
-    //remove this if you don't need it
-    let today = new Date(),
-        dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
-        nextYear = yyyy + 1,
-        dayMonth = "09/30/",
-        birthday = dayMonth + yyyy;
-
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-        birthday = dayMonth + nextYear;
-    }
-    //end
-
-    const countDown = new Date(birthday).getTime(),
+    
+    const countDown = new Date('12/09/2023').getTime(),
         x = setInterval(function () {
 
             const now = new Date().getTime(),
